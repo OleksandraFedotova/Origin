e27781277ffad37d325b3dcd97a60f0bdbfa4c92
Alexandra Fedotova <feyasasha19111996@gmail.com>
2016-05-24 23:50:32 +0300

    [test-app]
    call User Method

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Test1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Console.Write("hELLO {0}",new Program().Hello(Convert.ToString(Console.ReadLine())));
+            int a =Convert.ToInt16(Console.ReadLine());
+            string b= Convert.ToString(Console.ReadLine());
+            User A=new User(b,a);
+           Console.Write(A.GetInfo(A));
+        }
+
+        public string  Hello(string name)
+        {
+            return "Hello," + name + "!";
         }
     }
 }
