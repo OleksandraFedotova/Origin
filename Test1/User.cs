247eb943cc4002ec07403ea78f6d19131aceb544
Alexandra Fedotova <feyasasha19111996@gmail.com>
2016-05-24 23:42:03 +0300

    [test-app] created new class

---
+++ Test1/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class User
+    {
+         string name;
+        private int id;
+
+        public string GetInfo(User a)
+        {
+            return "user with id" + a.id + "is" + a.name;
+        }
+    }
+
+
+}
